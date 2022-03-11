4af275ddcec6afce00f8e7e725bccce9713209ed
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-03-11 14:20:04 +0000

    Diskussion static

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,13 +1,25 @@
 package com.cc.java;
+
 public class App {
     public static void main(String[] args) {
       
-     Level_0 player = new Level_0();
-     ausgabe(player.play());  
+    //  Level_0 player = new Level_0();
+    //  ausgabe(player.play());  
+
+        ausgabe(String.valueOf(2));
+        ausgabe(2);
+        // 1. Typumwandlung int --> String
+        // 2. Overloading
 
     }
 
     private static void ausgabe(String outStr) {
         System.out.println(outStr);
     }
+
+    private static void ausgabe(int boo) {
+        System.out.println(boo);
+    }
+
+
 }
